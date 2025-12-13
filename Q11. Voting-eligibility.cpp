26fad65180f445004b2787aee46a5c82af668566
Mr Zinna <141651533+unknownxx69@users.noreply.github.com>
2025-12-13 23:39:41 +0530

    Add files via upload

---
+++ Q11. Voting-eligibility.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int age;
+    cout<<"Enter the age: ";
+    cin>>age;
+    if (age>=18) {
+        cout<<"Eligible to vote";
+    }
+    else {
+        cout<<"Not Eligible to vote";
+    }
+    return 0;
+}
